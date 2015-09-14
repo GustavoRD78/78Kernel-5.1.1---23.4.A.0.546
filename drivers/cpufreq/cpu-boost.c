ba610a9fb1db7429f9dcb770053237b3eb429ad0
GustavoRD78 <rd13578@gmail.com>
2015-09-13 21:02:52 -0300

    Revert "cpufreq: cpu-boost: allow enable/disable via sysfs (test battery life is need)"
    This reverts commit 1d254af658f9d0f21b76d8fed0cb3dfacdeeaf12.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -45,9 +45,6 @@ static struct workqueue_struct *cpu_boost_wq;
 
 static struct work_struct input_boost_work;
 
-bool cpuboost_enable = true;
-module_param(cpuboost_enable, bool, 0644);
-
 static unsigned int boost_ms;
 module_param(boost_ms, uint, 0644);
 
@@ -82,8 +79,6 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val, voi
 	unsigned int b_min = s->boost_min;
 	unsigned int ib_min = s->input_boost_min;
 	unsigned int min;
-
-	if (!cpuboost_enable) return NOTIFY_OK;
 
 	switch (val) {
 	case CPUFREQ_ADJUST:
@@ -145,8 +140,6 @@ static int boost_mig_sync_thread(void *data)
 	struct cpufreq_policy src_policy;
 	unsigned long flags;
 
-	if (!cpuboost_enable) return 0;
-
 	while(1) {
 		wait_event_interruptible(s->sync_wq, s->pending ||
 					kthread_should_stop());
@@ -206,8 +199,6 @@ static int boost_migration_notify(struct notifier_block *nb,
 	unsigned long flags;
 	struct cpu_sync *s = &per_cpu(sync_info, dest_cpu);
 
-	if (!cpuboost_enable) return NOTIFY_OK;
-
 	if (!boost_ms)
 		return NOTIFY_OK;
 
@@ -240,8 +231,6 @@ static void do_input_boost(struct work_struct *work)
 	struct cpu_sync *i_sync_info;
 	struct cpufreq_policy policy;
 
-	if (!cpuboost_enable) return;
-
 	get_online_cpus();
 	for_each_online_cpu(i) {
 
@@ -266,8 +255,6 @@ static void cpuboost_input_event(struct input_handle *handle,
 		unsigned int type, unsigned int code, int value)
 {
 	u64 now;
-
-	if (!cpuboost_enable) return;
 
 	if (!input_boost_freq)
 		return;
